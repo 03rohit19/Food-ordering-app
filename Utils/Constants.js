2a5af0863675375d403a90d72f03922a055a39a6
03rohit19 <rohitfriday01@gmail.com>
2023-06-15 11:06:13 +0530

    Utils file added

---
+++ Utils/Constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://www.designmantic.com/logo-images/5807.png?company=Company+Name&slogan=&verify=1";
